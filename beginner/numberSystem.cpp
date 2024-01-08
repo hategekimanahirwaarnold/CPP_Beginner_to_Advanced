8b1dd36f9979b78e6b677b44416df8345e85b538
Hirwa Arnold <macbook@macbooks-MacBook-Pro.local>
2024-01-08 18:45:38 +0200

    Edit comments

--- beginner/numberSystem.cpp
+++ beginner/numberSystem.cpp
@@ -8,7 +8,7 @@ int main() {
     int hexn = 0xf;
     cout << "hex: " << hexn << endl;
     
-    unsigned int nbr = 255; // unsigned makes it must for a nbr to be positive. This comes at the expanse of printing a bigger number when nbr < 0;
+    unsigned int nbr = 255; // unsigned makes it a must for a nbr to be positive. This comes at the expanse of printing a bigger number when nbr < 0;
     cout << "un nbr: "<< nbr << endl;
 
     return 0;
