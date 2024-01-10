b59584b6b3dc843c9e13fd7dc9ce5fecce509d95
Hirwa Arnold <macbook@macbooks-MacBook-Pro.local>
2024-01-10 22:27:27 +0200

    Polymorphism

---
+++ advanced/21-Polymorphism/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+/**
+ * Polymorphism:
+ * -------------
+ * Managing derived objects in memory through base pointers or
+ * references and getting the right method called on the base pointer or reference.
+*/
+int main()
+{
+
+    return 0;
+}
